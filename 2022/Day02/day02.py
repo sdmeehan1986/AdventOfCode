605476f7e7cbf0c330a2a2e9c493013c37167507
sdmeehan1986 <sdmeehan@hotmail.co.uk>
2023-11-21 16:02:29 +0000

    Complete Day 2

---
+++ 2022/Day02/day02.py
@@ -0,0 +1,55 @@
+def get_input(file_name):
+    with open(file_name) as file:
+        return file.readlines()
+
+def sides(data):
+    return [int(x.rstrip()) for x in data.split("x")]
+
+def test():
+    print("Starting test run 1")
+    data = get_input("test.txt")
+    total = 0
+    ribbon = 0
+
+    for i in data:
+        l, w, h = sides(i)
+        area = [l * w, w * h, h * l]
+        smallest = area
+        smallest.sort()
+        total += sum([x * 2 for x in area]) + smallest[0]
+
+        check = [l, w, h]
+        check.sort()
+        first = check[0]
+        second = check[1]
+        ribbon += (first * 2) + (second * 2) + (l * w * h)
+
+    print(f'The total wrapping paper required is {total}')
+    print(f'The total ribbon required is {ribbon}')
+    print("---------------")
+
+def live():
+    print("Starting live run 1")
+    data = get_input("input.txt")
+    total = 0
+    ribbon = 0
+    for i in data:
+        l, w, h = sides(i)
+        area = [l * w, w * h, h * l]
+        smallest = area
+        smallest.sort()
+        total += sum([x * 2 for x in area]) + smallest[0]
+
+        check = [l, w, h]
+        check.sort()
+        first = check[0]
+        second = check[1]
+        ribbon += (first * 2) + (second * 2) + (l * w * h)
+
+    print(f'The total wrapping paper required is {total}')
+    print(f'The total ribbon required is {ribbon}')
+    print("---------------")
+
+
+test()
+live()
